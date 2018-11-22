b77662f47a9c7e09feeb9323341756b010ed7e9d
khalednakhleh <khaled.jkn@gmail.com>
2018-11-22 10:51:13 -0600

    modified shell script

--- start.sh
+++ start.sh
@@ -2,14 +2,40 @@
 #  Author: Khaled Nakhleh. 2018
 # To give permission, type in the terminal: chmod 755 <filename>
 
-echo "Running:" $0
-
-echo "Making/overwriting directories for saving pictures ...\n"
-
-mkdir camera_1
-mkdir camera_2
-
+function Capturing {
 echo "Running ReadCam.py...\n"
 python ReadCam.py
+}
 
-exit
+echo "Running:" $0
+
+if 
+test -d camera_1 && test -d camera_2
+then
+echo "Directories camera_1 and camera_2 exist."
+else
+echo "Directories camera_1 and camera_2 don't exist. Creating them..."
+mkdir camera_1 camera_2
+fi
+
+valid=0
+echo "Start capturing? [y/n]"
+while 
+[ $valid == 0 ]
+do 
+read ans
+case $ans in 
+yes|YES|y|Y|yup|YUP) 
+             echo "Starting..."
+             Capturing
+             valid=1;;
+no|NO|n|N|nope|NOPE)
+            echo "Shutting down..."
+            valid=1
+            exit;;
+*           )
+            echo "type yes or no in any form";;
+esac
+done
+
+
